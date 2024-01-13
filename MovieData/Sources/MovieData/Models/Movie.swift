41454de9afb5b3e5f5a7b859726acc2d4d9aa37e
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2024-01-13 20:09:40 +0100

    Add MovieRepository; Add test for URL coding

---
+++ MovieData/Sources/MovieData/Models/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//
+//
+//  Created by Tibor Felföldy on 2024-01-13.
+//
+
+public struct Movie: Decodable {}
---
+++ MovieData/Sources/MovieData/MovieRepository.swift
@@ -0,0 +1,54 @@
+//
+//  MovieRepository.swift
+//
+//
+//  Created by Tibor Felföldy on 2024-01-13.
+//
+
+import Foundation
+
+public protocol MovieRepository {
+    func fetchMovies() async throws -> [Movie]
+}
+
+struct FetchMoviesResponse: Decodable {
+    let results: [Movie]
+}
+
+final class DefaultMovieRepository: MovieRepository {
+    enum Endpoint: String {
+        case popular = "/movie/popular"
+    }
+    
+    private let apiKey: String
+    private let baseURL = "https://api.themoviedb.org/3"
+    
+    init(apiKey: String) {
+        self.apiKey = apiKey
+    }
+    
+    func fetchMovies() async throws -> [Movie] {
+        let url = url(for: .popular)
+        // Request.
+        let (data, _) = try await URLSession.shared.data(from: url)
+        // Decode.
+        let response = try decode(data: data)
+        return response.results
+    }
+    
+    func url(for endpoint: Endpoint) -> URL {
+        var components = URLComponents(string: baseURL + endpoint.rawValue)
+        
+        components?.queryItems = [
+            URLQueryItem(name: "api_key", value: apiKey)
+        ]
+
+        return components!.url!
+    }
+    
+    func decode(data: Data) throws -> FetchMoviesResponse {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode(FetchMoviesResponse.self, from: data)
+    }
+}
